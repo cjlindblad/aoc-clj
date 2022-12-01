e979eb928b21a6393c540003495c3816b31d5eeb
CJ Lindblad <carl-johan.lindblad@svt.se>
2022-12-01 20:49:05 +0100

    Cleaned up 2022 day 1.

--- src/aoc_clj/2022/01/solution.clj
+++ src/aoc_clj/2022/01/solution.clj
@@ -1,24 +1,20 @@
 (ns aoc-clj.2022.01.solution
   (:require [clojure.string :as string]))
 
-(def input (string/split-lines (slurp "src/aoc_clj/2022/01/input.txt")))
-(def test-input (string/split-lines (slurp "src/aoc_clj/2022/01/test-input.txt")))
-
-(defn parse-int [n] (Integer/parseInt n))
+(def input (slurp "src/aoc_clj/2022/01/input.txt"))
+(def test-input (slurp "src/aoc_clj/2022/01/test-input.txt"))
 
 (defn mapf [f' f coll] (map (fn [el] (f' f el)) coll))
 (def mapmap (partial mapf map))
 (def mapreduce (partial mapf reduce))
 
 (defn calories-by-elf [input]
-  (let [string-groups (->> (partition-by empty? input)
-                           (filter #(not= [""] %)))
-        number-groups (mapmap parse-int string-groups)
-        sum-groups (mapreduce + number-groups)]
-    sum-groups))
+  (->> (string/split input #"\n\n")
+       (map #(string/split % #"\n"))
+       (mapmap #(Integer/parseInt %))
+       (mapreduce +)))
 
-(defn part-1 [input]
-  (apply max (calories-by-elf input)))
+(defn part-1 [input] (apply max (calories-by-elf input)))
 
 (defn part-2 [input]
   (->> (sort (calories-by-elf input))
