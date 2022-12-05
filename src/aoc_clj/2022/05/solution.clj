151930c68996d2eee4a4ed131eab9ae45979a225
CJ Lindblad <carl-johan.lindblad@svt.se>
2022-12-05 12:57:53 +0100

    Refactored 2022 day 5.

--- src/aoc_clj/2022/05/solution.clj
+++ src/aoc_clj/2022/05/solution.clj
@@ -5,33 +5,25 @@
 (def test-input (string/split-lines (slurp "src/aoc_clj/2022/05/test-input.txt")))
 
 (defn parse-instruction [line]
-  (let [[amount from to] (re-seq #"\d+" line)]
-    {:amount (Integer/parseInt amount)
-     :from (Integer/parseInt from)
-     :to (Integer/parseInt to)}))
-
-(defn parse-stack [stack-lines]
-  (let [stack (->> (apply map vector stack-lines)
-                   (map reverse)
-                   (filter #(Character/isDigit (first %)))
-                   (map rest)
-                   (map (partial filter #(not= \space %))))]
-    (zipmap (map inc (range)) stack)))
+  (map #(Integer/parseInt %) (re-seq #"\d+" line)))
 
 (defn parse-input [input]
-  (let [[raw-stacks _ instructions] (partition-by #(pos? (count %)) input)]
-    {:stacks (parse-stack raw-stacks)
-     :instructions (map parse-instruction instructions)}))
+  (let [[stack-lines _ instrs] (partition-by #(pos? (count %)) input)]
+    [(->> (apply map vector stack-lines)
+          (map reverse)
+          (filter #(Character/isDigit (first %)))
+          (map (partial filter #(not= \space %)))
+          (zipmap (map inc (range))))
+     (map parse-instruction instrs)]))
 
-(defn next-stacks [variant stacks {:keys [amount from to]}]
+(defn next-stacks [variant stacks [amount from to]]
   (let [to-move (take-last amount (stacks from))
         removed-stack {from (drop-last amount (stacks from))}
         added-stack {to (concat (stacks to) (variant to-move))}]
     (merge stacks removed-stack added-stack)))
 
 (defn solver [variant input]
-  (let [{:keys [stacks instructions]} (parse-input input)
-        final-state (reduce (partial next-stacks variant) stacks instructions)
+  (let [final-state (apply reduce (partial next-stacks variant) (parse-input input))
         ordered-stacks (map final-state (take (count final-state) (map inc (range))))]
     (apply str (map last ordered-stacks))))
 
