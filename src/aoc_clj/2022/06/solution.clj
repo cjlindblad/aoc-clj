76cead0d74e25dc213ef60cc8c77796991d35d2e
CJ Lindblad <carl-johan.lindblad@svt.se>
2022-12-06 07:07:04 +0100

    Changed a name.

--- src/aoc_clj/2022/06/solution.clj
+++ src/aoc_clj/2022/06/solution.clj
@@ -5,10 +5,10 @@
 
 (defn solver
   ([marker-length s] (solver marker-length s 0))
-  ([marker-length s chars]
+  ([marker-length s char-count]
    (let [candidate (take marker-length s)]
-     (if (apply distinct? candidate) (+ chars marker-length)
-         (recur marker-length (rest s) (inc chars))))))
+     (if (apply distinct? candidate) (+ char-count marker-length)
+         (recur marker-length (rest s) (inc char-count))))))
 
 (def part-1 (partial solver 4))
 (def part-2 (partial solver 14))
