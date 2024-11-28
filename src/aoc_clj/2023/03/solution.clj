a69ea2db84d412e9b73f95d27f9721dd1ddbb6d0
CJ Lindblad <carljohan.lindblad@gmail.com>
2024-11-28 21:21:50 +0100

    2023 day 3 part 2.

--- src/aoc_clj/2023/03/solution.clj
+++ src/aoc_clj/2023/03/solution.clj
@@ -9,6 +9,13 @@
        (apply interleave)
        (partition 2)))
 
+(defn parse-input-2 [input]
+  (->> (str/split input #"\n")
+       (map #(re-seq #"\d+" %))
+       (map (partial apply str))
+       (map parse-long)
+       (conj [])))
+
 (defn distances [time]
   (for [speed (range (inc time))]
     (* speed (- time speed))))
@@ -16,11 +23,18 @@
 (defn win-options [[time record]]
   (filter (partial < record) (distances time)))
 
+(defn solver [input]
+  (->>
+   (map win-options input)
+   (map count)
+   (apply *)))
+
 (defn part-1 [input]
-  (->> (parse-input input)
-       (map win-options)
-       (map count)
-       (apply *)))
+  (solver (parse-input input)))
+
+(defn part-2 [input]
+  (solver (parse-input-2 input)))
 
 (comment
-  (= 2374848 (part-1 input)))
+  (= 2374848 (part-1 input))
+  (= 39132886 (part-2 input)))
