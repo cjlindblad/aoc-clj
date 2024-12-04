fdd1952a0065e897c464594fd55a3c19286d863a
Carl-Johan Lindblad <carl-johan.lindblad@sr.se>
2024-12-04 13:09:09 +0100

    Refactored 2024 day 04 a bit.

--- src/aoc_clj/2024/04/solution.clj
+++ src/aoc_clj/2024/04/solution.clj
@@ -4,6 +4,9 @@
 (def input (str/split (slurp "src/aoc_clj/2024/04/input.txt") #"\n"))
 (def test-input (str/split (slurp "src/aoc_clj/2024/04/test-input.txt") #"\n"))
 
+(defn in-bounds [size]
+  (fn [[x y]] (and (<= 0 x) (<= 0 y) (< x size) (< y size))))
+
 (defn get-coord-ranges [[x y] size]
   (let [deltas [[[0 0] [1 0] [2 0] [3 0]]
                 [[0 0] [-1 0] [-2 0] [-3 0]]
@@ -14,19 +17,13 @@
                 [[0 0] [-1 -1] [-2 -2] [-3 -3]]
                 [[0 0] [1 -1] [2 -2] [3 -3]]]]
     (->> (map (partial map (fn [[dx dy]] [(+ dx x) (+ dy y)])) deltas)
-         (map (partial filter (fn [[x y]] (and (<= 0 x)
-                                               (<= 0 y)
-                                               (< x size)
-                                               (< y size)))))
+         (map (partial filter (in-bounds size)))
          (filter #(= 4 (count %))))))
 
 (defn get-coord-ranges-2 [[x y] size]
   (->> [[0 0] [-1 -1] [1 -1] [-1 1] [1 1]]
        (map (fn [[dx dy]] [(+ dx x) (+ dy y)]))
-       (filter (fn [[x y]] (and (<= 0 x)
-                                (<= 0 y)
-                                (< x size)
-                                (< y size))))))
+       (filter (in-bounds size))))
 
 (defn get-word [coord-range input]
   (->> (map (fn [[x y]] (get-in input [y x])) coord-range)
@@ -34,8 +31,7 @@
 
 (defn part-1 [input]
   (let [size (count input)
-        coords (for [x (range 0 size)
-                     y (range 0 size)] [x y])
+        coords (for [x (range 0 size) y (range 0 size)] [x y])
         coord-ranges (map #(get-coord-ranges % size) coords)
         words (mapcat (partial map #(get-word % input)) coord-ranges)
         xmases (filter #{"XMAS"} words)]
@@ -43,8 +39,7 @@
 
 (defn part-2 [input]
   (let [size (count input)
-        coords (for [x (range 0 size)
-                     y (range 0 size)] [x y])
+        coords (for [x (range 0 size) y (range 0 size)] [x y])
         coord-ranges (map #(get-coord-ranges-2 % size) coords)
         words (map #(get-word % input) coord-ranges)
         x-mases (filter #{"ASSMM" "ASMSM" "AMSMS" "AMMSS"} words)]
