c04076ca25fbc5ff68ec362a70fd2925560ef821
Carl-Johan Lindblad <carl-johan.lindblad@sr.se>
2024-12-05 12:50:24 +0100

    2024 day 02.

--- src/aoc_clj/2024/05/solution.clj
+++ src/aoc_clj/2024/05/solution.clj
@@ -35,7 +35,31 @@
         middles (map middle valid-lines)]
     (reduce + middles)))
 
+(defn correct [line [left-rule right-rule]]
+  (let [left-index (.indexOf line left-rule)
+        right-index (.indexOf line right-rule)]
+    (if (or (neg? left-index) (neg? right-index) (< left-index right-index))
+      line
+      (-> (assoc line left-index right-rule)
+          (assoc right-index left-rule)))))
+
+(defn apply-rules [line all-rules]
+  (loop [line line rules all-rules]
+    (if (seq rules)
+      (recur (correct line (first rules)) (rest rules))
+      (if (all-rules-valid? all-rules line)
+        line
+        (apply-rules line all-rules)))))
+
+(defn part-2 [input]
+  (let [{:keys [rules lines]} (parse-input input)
+        invalid-lines (filter (complement (partial all-rules-valid? rules)) lines)
+        corrected-lines (map (fn [line] (apply-rules line rules)) invalid-lines)
+        middles (map middle corrected-lines)]
+    (reduce + middles)))
+
 (comment
-  (= 5732 (part-1 input)))
+  (= 5732 (part-1 input))
+  (= 4716 (part-2 input)))
 
 
