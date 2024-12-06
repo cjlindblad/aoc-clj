61a98f74304bab624bde58bef6fd2cbe60602374
Carl-Johan Lindblad <carl-johan.lindblad@sr.se>
2024-12-06 09:44:29 +0100

    2024 day 06 part 2.

--- src/aoc_clj/2024/06/solution.clj
+++ src/aoc_clj/2024/06/solution.clj
@@ -40,8 +40,10 @@
 (defn move [area {:keys [x y direction inside] :as pos}]
   (let [blocked (blocked? area pos)
         next-direction (if blocked (turn-right direction) direction)
-        [next-x next-y] (next-pos {:x x :y y :direction next-direction})]
-    {:x next-x :y next-y :direction next-direction :inside (inside? area [next-x next-y])}))
+        really-blocked (blocked? area (assoc pos :direction next-direction))
+        final-direction (if really-blocked (turn-right next-direction) next-direction)
+        [next-x next-y] (next-pos {:x x :y y :direction final-direction})]
+    {:x next-x :y next-y :direction final-direction :inside (inside? area [next-x next-y])}))
 
 (defn walk [area pos]
   (loop [current-pos pos visited [pos]]
@@ -59,6 +61,24 @@
   (-> (walk input (starting-pos input))
       distinct-locations))
 
+(defn loop? [area pos]
+  (loop [current-pos pos visited #{pos}]
+    (let [npos (move area current-pos)]
+      (if (not (:inside npos))
+        false
+        (if (visited npos) true
+            (recur npos (conj visited npos)))))))
+
+(defn part-2 [input]
+  (let [positions (rest (walk input (starting-pos input)))
+        area (mapv (partial mapv identity) input)
+        candidates (distinct (map (fn [{:keys [x y]}] (assoc-in area [y x] \#)) positions))
+        start-pos (starting-pos input)
+        ;; we could be smarter about starting position here
+        loops (filter (fn [candidate] (loop? candidate (assoc start-pos :inside true))) candidates)]
+    (count loops)))
+
 (comment
-  (= 4977 (part-1 input)))
+  (= 4977 (part-1 input))
+  (= 1729 (part-2 input)))
 
