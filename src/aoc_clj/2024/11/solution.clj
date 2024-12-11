a5c8eb1d8cd89e0aadfcb2da2c59459864f679eb
Carl-Johan Lindblad <carl-johan.lindblad@sr.se>
2024-12-11 14:32:49 +0100

    Cleanup.

--- src/aoc_clj/2024/11/solution.clj
+++ src/aoc_clj/2024/11/solution.clj
@@ -3,31 +3,16 @@
 (def input (read-string (str "[" (slurp "src/aoc_clj/2024/11/input.txt") "]")))
 (def test-input [125 17])
 
-(defn digits [n]
-  (loop [n n i 0]
-    (if (zero? n)
-      i
-      (recur (quot n 10) (inc i)))))
-
-(defn pow [b e]
-  (loop [n b e (dec e)]
-    (if (zero? e)
-      n
-      (recur (* n b) (dec e)))))
-
-(defn split [n digits]
-  (let [factor (pow 10 (/ digits 2))
-        left (quot n factor)
-        right (- n (* left factor))]
-    [left right]))
+(defn parse-long [n] (Long/parseLong n))
 
 (defn apply-rule [stone]
-  (let [length (digits stone)
+  (let [stone-str (str stone)
+        length (count stone-str)
         even-digits (even? length)]
     (cond
       (zero? stone) [1]
-      even-digits (let [[left right] (split stone length)]
-                    [left right])
+      even-digits (let [[left right] (partition (/ length 2) stone-str)]
+                    [(parse-long (apply str left)) (parse-long (apply str right))])
       :else [(* stone 2024)])))
 
 (defn stones->stone-count [stones]
@@ -36,26 +21,17 @@
        (into {})))
 
 (defn solver [times stones]
-  (->> (let [stone-count-atom (atom (stones->stone-count stones))]
-         (loop [times times stone-count-atom stone-count-atom]
-           (let [next-atom (atom {})]
-             (if (zero? times)
-               stone-count-atom
-               (do
-                 (dorun
-                  (map
-                   (fn [[stone stone-count]]
-                     (let [expanded (apply-rule stone)]
-                       (dorun
-                        (map
-                         (fn [next-stone]
-                           (swap! next-atom update next-stone (fnil + 0) stone-count))
-                         expanded))))
-                   @stone-count-atom))
-                 (recur (dec times) next-atom))))))
-       deref
-       (map last)
-       (reduce +)))
+  (->>  (loop [times times current-atom (atom (stones->stone-count stones))]
+          (if (zero? times)
+            @current-atom
+            (let [next-atom (atom {})]
+              (doseq [[stone stone-count] @current-atom]
+                (let [expanded (apply-rule stone)]
+                  (doseq [next-stone expanded]
+                    (swap! next-atom update next-stone (fnil + 0) stone-count))))
+              (recur (dec times) next-atom))))
+        (map last)
+        (reduce +)))
 
 (def part-1 (partial solver 25))
 (def part-2 (partial solver 75))
