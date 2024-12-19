077a9eacef66c9c45eb9f4e595626d4761fe86ce
Carl-Johan Lindblad <carl-johan.lindblad@sr.se>
2024-12-19 16:56:09 +0100

    2024 day 19 part 2.

--- src/aoc_clj/2024/19/solution.clj
+++ src/aoc_clj/2024/19/solution.clj
@@ -1,9 +1,5 @@
 (ns aoc-clj.2024.19.solution
-  (:require [clojure.string :as str]
-            [clojure.set :as set]
-            [loom.graph :as graph]
-            [loom.alg :as alg]
-            [loom.io :as io]))
+  (:require [clojure.string :as str]))
 
 (def input (slurp "src/aoc_clj/2024/19/input.txt"))
 (def test-input (slurp "src/aoc_clj/2024/19/test-input.txt"))
@@ -13,53 +9,28 @@
     {:towels (str/split towels #", ")
      :designs (str/split designs #"\n")}))
 
-(defn buildable? [dictionary towel]
-  (let [n (count towel)
-        dp-atom (atom (vec (concat [true] (repeat n false))))]
+(defn word-break [s word-set]
+  (let [n (count s)
+        dp (atom (vec (repeat (inc n) 0)))]
+    (swap! dp assoc 0 1)
     (doseq [i (range 1 (inc n))]
       (doseq [j (range i)]
-        (let [substring (subs towel j i)]
-          (when (and (@dp-atom j)
-                     (dictionary substring))
-            (swap! dp-atom assoc i true)))))
-    (@dp-atom n)))
+        (when (word-set (subs s j i))
+          (let [old (@dp i)
+                add (@dp j)]
+            (swap! dp assoc i (+ old add))))))
+    (@dp n)))
 
-(defn part-1 [input]
+(defn solver [input]
   (let [{:keys [towels designs]} (parse-input input)
-        dictionary (into #{} towels)]
-    (->> (filter (fn [towel] (buildable? dictionary towel)) designs)
-         count)))
+        word-set (into #{} towels)]
+    (->> (map (fn [design] (word-break design word-set)) designs)
+         (filter pos?))))
 
-(defn arrangements-recursive [dictionary remaining memo]
-  (if (@memo remaining)
-    (@memo remaining)
-    (if-not (seq remaining)
-      [""]
-      (let [result (atom [])]
-        (doseq [i (range 1 (inc (count remaining)))]
-          (let [prefix (subs remaining 0 i)]
-            (when (dictionary prefix)
-              (let [suffix-ways (arrangements-recursive dictionary (subs remaining i) memo)]
-                (doseq [way suffix-ways]
-                  (if (= "" way)
-                    (swap! result conj prefix)
-                    (swap! result conj (str prefix " " way))))))))
+(defn part-1 [input] (count (solver input)))
 
-        (swap! memo assoc remaining @result)
-        @result))))
+(defn part-2 [input] (reduce + (solver input)))
 
-(defn arrangements [dictionary design]
-  (arrangements-recursive dictionary design (atom {})))
-
-(defn part-2 [input]
-  (let [{:keys [towels designs]} (parse-input input)
-        dictionary (into #{} towels)
-        valid-designs (filter (fn [towel] (buildable? dictionary towel)) designs)]
-    ;(arrangements dictionary (first valid-designs))
-    (->> (map (fn [design] (arrangements dictionary design)) valid-designs)
-         (map count)
-         (reduce +))))
-
-(part-2 input)
-
-;(part-1 input) ; 209
+(comment
+  (= 209 (part-1 input))
+  (= 777669668613191 (part-2 input)))
