e9e5e2e42c522f28639200190f5e291016332d70
CJ Lindblad <carljohan.lindblad@gmail.com>
2024-12-27 16:14:24 +0100

    Realized what I have to do..

--- src/aoc_clj/2024/21/solution.clj
+++ src/aoc_clj/2024/21/solution.clj
@@ -8,10 +8,15 @@
 (def input (slurp "src/aoc_clj/2024/21/input.txt"))
 (def test-input (slurp "src/aoc_clj/2024/21/test-input.txt"))
 
+(defn parse-long [n] (Long/parseLong n))
+
 (def numpad ["789"
              "456"
              "123"
              " 0A"])
+
+(def dirpad [" ^A"
+             "<v>"])
 
 (defn coords->direction [from to]
   (let [[dx dy] (mapv - to from)]
@@ -21,7 +26,7 @@
       [1 0] \>
       [-1 0] \<)))
 
-(defn build-graph-data [lines]
+(defn build-graph-data [lines valid-chars]
   (let [start-coord (atom nil)
         graph-data
         (as->
@@ -29,10 +34,10 @@
           identity
           (for [y (range (count lines))
                 x (range (count (first lines)))
-                :when (#{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \A} (get-in lines [y x]))]
+                :when (valid-chars (get-in lines [y x]))]
             (let [current-char (get-in lines [y x])
                   neighbours (mapv (partial mapv + [x y]) [[1 0] [0 1] [-1 0] [0 -1]])
-                  valid-neighbours (vec (filter (fn [[x y]] (#{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \A} (get-in lines [y x]))) neighbours))]
+                  valid-neighbours (vec (filter (fn [[x y]] (valid-chars (get-in lines [y x]))) neighbours))]
               (when (= \A current-char)
                 (reset! start-coord [current-char]))
               (for [direction [\^ \> \v \<]]
@@ -71,7 +76,7 @@
     (apply graph/add-edges g nodes-to-add)))
 
 (defn numpad-path [from to]
-  (let [numpad-graph-data (build-graph-data numpad)
+  (let [numpad-graph-data (build-graph-data numpad #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \A})
         numpad-graph (graph/weighted-digraph numpad-graph-data)
         with-start (add-start-node numpad-graph from)
         with-goal (add-goal-node with-start to)]
@@ -87,5 +92,49 @@
          (map (partial apply str))
          (apply str))))
 
-(numpad-sequence "029A")
+;; TODO could be parameterized and merged with numpad-path
+(defn dirpad-path [from to]
+  (let [dirpad-graph-data (build-graph-data dirpad #{\< \^ \> \v \A})
+        dirpad-graph (graph/weighted-digraph dirpad-graph-data)
+        with-start (add-start-node dirpad-graph from)
+        with-goal (add-goal-node with-start to)]
+    (->> (alg/dijkstra-path with-goal :start :goal)
+         (drop 2)
+         (drop-last 1)
+         (mapv last))))
+
+(defn dirpad-sequence [ds]
+  (let [from-tos (partition 2 1 (concat [\A] ds))]
+    (->> (map (fn [[from to]] (dirpad-path from to)) from-tos)
+         (mapv (fn [x] (concat x [\A])))
+         (map (partial apply str))
+         (apply str))))
+
+(defn code-complexity [code]
+  (let [seq-1 (numpad-sequence code)
+        seq-2 (dirpad-sequence seq-1)
+        seq-3 (dirpad-sequence seq-2)
+        numeric-part (parse-long (subs code 0 3))]
+    (* numeric-part (count seq-3))))
+
+(defn part-1 [input]
+  (->> (str/split input #"\n")
+       (map code-complexity)
+       (reduce +)))
+
+; TODO we need to find ALL shortest paths for each step, and check which is the shortest after all expansions. Solve day 16 first?
+(part-1 test-input)
+; 185828 too high
+; (["029A" 68 29]
+;  ["980A" 60 980]
+;  ["179A" 68 179]
+;  ["456A" 64 456]
+;  ["379A" 68 379])
+
+(code-complexity "029A")
+
+(dirpad-sequence (dirpad-sequence (numpad-sequence "379A")))
+; "^A^^<<A>>AvvvA"
+; "<A>A<AAv<AA>>^AvAA^A<vAAA^>A"
+; "v<<A>>^AvA^Av<<A>>^AA<vA<A>>^AAvAA^<A>A<vA^>AA<A>Av<<A>A^>AAA<Av>A^A"
 
