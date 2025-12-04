5c870788a6311be985e98b94d5eddf79d46e1378
CJ Lindblad <carljohan.lindblad@gmail.com>
2025-12-04 20:54:31 +0100

    2025 day 04 part 2.

--- src/aoc_clj/2025/04/solution.clj
+++ src/aoc_clj/2025/04/solution.clj
@@ -1,5 +1,6 @@
 (ns aoc-clj.2025.04.solution
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def input (slurp "src/aoc_clj/2025/04/input.txt"))
 (def test-input (slurp "src/aoc_clj/2025/04/test-input.txt"))
@@ -34,5 +35,23 @@
       paper-coords)
      count)))
 
+(defn solver-2
+  ([input] (solver-2 (parse-input input) 0 true))
+  ([paper-coords removed-paper-rolls did-remove-rolls]
+   (if (false? did-remove-rolls)
+     removed-paper-rolls
+     (let [removable-paper-rolls (->> (filter
+                                       (fn [[x y]]
+                                         (let [neighbour-coords (get-neighbour-coords [x y])
+                                               paper-roll-neighbours (filter paper-coords neighbour-coords)]
+                                           (< (count paper-roll-neighbours) 4)))
+                                       paper-coords)
+                                      (into #{}))]
+       (recur
+        (set/difference paper-coords removable-paper-rolls)
+        (+ removed-paper-rolls (count removable-paper-rolls))
+        (> (count removable-paper-rolls) 0))))))
+
 (comment
-  (= 1393 (solver-1 input)))
+  (= 1393 (solver-1 input))
+  (= 8643 (solver-2 input)))
